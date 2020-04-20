5332518ddca1ab2e0dc434df9445e58aa08f0b02
Hamcha <victor.hamcha@gmail.com>
2020-04-20 15:04:52 +0200

    Player Prefab

---
+++ GGweek/Assets/Script/Victor/RigidBodyMouvement.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+
+namespace Player
+{
+    public class RigidBodyMouvement : MonoBehaviour
+    {
+        [Header("Movement")]
+        public float movementSpeed;
+        public float airMovementSpeed;
+        public float maxSpeed;
+
+        [Header("Friction")]
+        public float friction;
+        public float airFriction;
+
+
+        [Header("Gravity")]
+        public float extraGravity;
+
+        [Header("Ground Detection")]
+        public LayerMask whatIsGround;
+        public float checkYOffset;
+        public float checkRadius;
+        public float groundTimer;
+
+        [Header("Jumping")]
+        public float jumpForce;
+        public float jumpCooldown;
+
+        [Header("Data")]
+        public Rigidbody _rb;
+
+
+
+        private float _grounded;
+        private bool _realGrounded;
+        private float _jumpCooldown;
+     
+     
+        private void Start()
+        {
+          
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            GroundCheck();
+            ApplyMovement();
+            ApplyFriction();
+            ApplyGravity();
+        }
+
+        private void Update()
+        {
+            Jumping();
+           
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.CheckCapsule(GetComponent<CapsuleCollider>().bounds.center, new Vector3(GetComponent<CapsuleCollider>().bounds.center.x, GetComponent<CapsuleCollider>().bounds.min.y, GetComponent<CapsuleCollider>().bounds.center.z), GetComponent<CapsuleCollider>().radius * 0.9f, whatIsGround);
+
+        }
+        private void GroundCheck()
+        {
+           _grounded -= Time.fixedDeltaTime;
+            var colliderList = new Collider[100];
+            var size = Physics.OverlapSphereNonAlloc(transform.position + new Vector3(0, checkYOffset, 0), checkRadius, colliderList, whatIsGround);
+            _realGrounded = size > 0;
+            if (_realGrounded)
+                _grounded = groundTimer;
+
+        }
+
+        private void ApplyMovement()
+        {
+            Vector2 axis = new Vector2(
+                Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical")
+            ).normalized;
+
+            float speed = _realGrounded ? movementSpeed : airMovementSpeed;
+            Vector3 vertical = axis.y * speed * Time.fixedDeltaTime * transform.forward;
+            Vector3 horizontal = axis.x * speed * Time.fixedDeltaTime * transform.right;
+           
+
+            
+                if (CanApplyForce(vertical, axis))
+                    _rb.velocity += vertical;
+
+                if (CanApplyForce(horizontal, axis))
+                    _rb.velocity += horizontal;
+            
+
+        }
+
+        private void ApplyFriction()
+        {
+            Vector3 vel = _rb.velocity;
+            float target = _realGrounded ? friction : airFriction;
+            vel.x = Mathf.Lerp(vel.x, 0f, target * Time.fixedDeltaTime);
+            vel.z = Mathf.Lerp(vel.z, 0f, target * Time.fixedDeltaTime);
+            _rb.velocity = vel;
+        }
+
+
+
+        private void ApplyGravity()
+        {
+            Vector3 vel = _rb.velocity;
+            vel.y -= Mathf.Abs(vel.y) * Time.fixedDeltaTime * extraGravity;
+            _rb.velocity = vel;
+        }
+
+        private void Jumping()
+        {
+
+            _jumpCooldown -= Time.deltaTime;
+            if (!IsGrounded() || !(_jumpCooldown <= 0) || !Input.GetKeyDown(KeyCode.Space)) return;
+            Vector3 vel = _rb.velocity;
+            vel.y = jumpForce;
+            _rb.velocity = vel;
+            _jumpCooldown = jumpCooldown;
+        }
+
+        
+
+       
+
+        private bool CanApplyForce(Vector3 target, Vector2 axis)
+        {
+            Vector2 targetC = Get2DVec(target).normalized;
+            Vector2 velocityC = Get2DVec(_rb.velocity).normalized;
+            float dotProduct = Vector2.Dot(velocityC, targetC);
+            return dotProduct <= 0 || dotProduct * Get2DVec(_rb.velocity).magnitude < maxSpeed * GetAxisForce(axis);
+        }
+
+        private static float GetAxisForce(Vector2 axis)
+        {
+            return (int)axis.x != 0 ? Mathf.Abs(axis.x) : Mathf.Abs(axis.y);
+        }
+
+        private static Vector2 Get2DVec(Vector3 vec)
+        {
+            return new Vector2(vec.x, vec.z);
+        }
+    }
+}
---
+++ GGweek/Assets/Script/Victor/ThreeDPlayerLooking.cs
@@ -0,0 +1,42 @@
+﻿
+//NORMAL//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeDPlayerLooking : MonoBehaviour
+{
+  
+    public float mouseSnesy = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+    public float minRotationHor;
+    public float MaxRotationHor;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+   
+
+
+    void Update()
+    {
+     
+        if (Time.timeScale != 0.0f)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSnesy;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSnesy;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+
+            
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+       
+    }
+}
+
