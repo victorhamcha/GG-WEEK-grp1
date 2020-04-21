cd1ce1e67457236e0a927742fa742daa63ec3c18
Benzakk <dovbenzak2@hotmail.com>
2020-04-21 12:22:33 +0200

    Creation inventaire + dossiers

---
+++ GGweek/Assets/Script/Dov/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryController : MonoBehaviour
+{
+    public Item item;
+
+    private void Start()
+    {
+        UpdateInfo();
+    }
+
+    public void UpdateInfo()
+    {
+        Image displayImage = transform.Find("Image").GetComponent<Image>();
+
+        if (item)
+        {
+            displayImage.sprite = item.icon;
+        }
+        else
+        {
+            displayImage.sprite = null;
+        }
+    }
+}
---
+++ GGweek/Assets/Script/Dov/InventoryMode.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMode : MonoBehaviour
+{
+    public static bool inventoryMode = false;
+
+    public GameObject inventoryUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (inventoryMode)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        inventoryUI.SetActive(false);
+        Time.timeScale = 1f;
+        inventoryMode = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause()
+    {
+        inventoryUI.SetActive(true);
+        Time.timeScale = 0f;
+        inventoryMode = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
