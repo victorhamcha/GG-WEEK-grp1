c5a78a1525a64efca649496c546759056fdaf0ef
Benzakk <dovbenzak2@hotmail.com>
2020-04-21 10:35:29 +0200

    Creation des Outline des objets

---
+++ GGweek/Assets/Script/Dov/Outline_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline_Controller : MonoBehaviour
+{
+    private MeshRenderer rendererr;
+
+    public float maxOutlineWidth;
+
+    public Color OutlineColor;
+
+    private void Start()
+    {
+        rendererr = GetComponent<MeshRenderer>();
+    }
+
+    public void ShouOutline()
+    {
+        rendererr.material.SetFloat("_Outline", maxOutlineWidth);
+        rendererr.material.SetColor("_OutlineColor", OutlineColor);
+    }
+
+    public void HideOutline()
+    {
+        rendererr.material.SetFloat("_Outline", 0f);
+    }
+}
---
+++ GGweek/Assets/Script/Dov/Outline_Lookat.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline_Lookat : MonoBehaviour
+{
+    public Camera cam;
+    public float dist;
+
+    private Outline_Controller prevController;
+    private Outline_Controller currentController;
+
+    private void Update()
+    {
+        HandleLookAtRay();
+    }
+
+    private void HandleLookAtRay()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, dist))
+        {
+            if (hit.collider.CompareTag("Interact"))
+            {
+                currentController = hit.collider.GetComponent<Outline_Controller>();
+
+                if (prevController != currentController)
+                {
+                    HideOutline();
+                    ShowOutline();
+                }
+                prevController = currentController;
+            }
+            else
+            {
+                HideOutline();
+            }
+        }
+        else
+        {
+            HideOutline();
+        }
+    }
+
+    private void ShowOutline()
+    {
+        if (currentController != null)
+        {
+            currentController.ShouOutline();
+        }
+    }
+
+    private void HideOutline()
+    {
+        if (prevController != null)
+        {
+            prevController.HideOutline();
+            prevController = null;
+        }
+    }
+}
