f09973b1908a7952a47ebb63842f42d7b43d2c91
Benzakk <dovbenzak2@hotmail.com>
2020-04-21 16:10:29 +0200

    Rectification outline + inventaire + liight

--- GGweek/Assets/Dov/Outline.shader
+++ GGweek/Assets/Dov/Outline.shader
@@ -4,7 +4,7 @@ Shader "Outlined/Diffuse" {
 	Properties{
 		_Color("Main Color", Color) = (.5,.5,.5,1)
 		_OutlineColor("Outline Color", Color) = (0,0,0,1)
-		_Outline("Outline width", Range(.002, 0.3)) = .005
+		_Outline("Outline width", Range(0.0, 0.3)) = .005
 		_MainTex("Base (RGB)", 2D) = "white" { }
 	}
 
---
+++ GGweek/Assets/Script/Dov/LightController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public GameObject obj;
+    public GameObject obj2;
+    private Light myLight;
+    private Light myLight2;
+
+    private void Start()
+    {
+        myLight = obj.GetComponent<Light>();
+        myLight2 = obj2.GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+            myLight2.enabled = !myLight2.enabled;
+        }
+    }
+}
