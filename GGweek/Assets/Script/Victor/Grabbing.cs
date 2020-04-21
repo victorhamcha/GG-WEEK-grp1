399d149032f01f4a953adfd70ed23ebb70789c8a
Hamcha <victor.hamcha@gmail.com>
2020-04-21 09:45:45 +0200

    Grabbing mid

--- GGweek/Assets/Script/Victor/Grabbing.cs
+++ GGweek/Assets/Script/Victor/Grabbing.cs
@@ -37,9 +37,11 @@ public class Grabbing : MonoBehaviour
     public GameObject Grab(GameObject grabbed)
     {
         Cursor.lockState = CursorLockMode.None;
-        //  grabbed.transform.parent = Camera.main.transform;
-        grabbed.transform.position = objPos.localPosition;
         Camera.main.transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, Camera.main.transform.eulerAngles.z);
+        grabbed.transform.parent = Camera.main.transform;
+        grabbed.transform.position = new Vector3(0, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, blur.transform.position.z);
+        grabbed.transform.localPosition = new Vector3(0, grabbed.transform.localPosition.y, 1);
+      
         grabbed.GetComponent<Rigidbody>().isKinematic = true;
         grabbed.GetComponent<Collider>().isTrigger = true;
         isGrabbing = true;
@@ -49,7 +51,7 @@ public class Grabbing : MonoBehaviour
     public void Drop(GameObject grabbed)
     {
         Cursor.lockState = CursorLockMode.Locked;
-       // grabbed.transform.parent = null;
+        grabbed.transform.parent = null;
         grabbed.GetComponent<Rigidbody>().isKinematic = false;
         grabbed.GetComponent<Collider>().isTrigger = false;
         isGrabbing = false;
