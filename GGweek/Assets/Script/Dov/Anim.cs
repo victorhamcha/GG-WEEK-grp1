5f14be168801267474f46846e195f4b1035da56a
Benzakk <dovbenzak2@hotmail.com>
2020-04-22 15:54:21 +0200

    Modif Animation

---
+++ GGweek/Assets/Script/Dov/Anim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    public Animator animShake;
+
+
+    void Update()
+    {
+        if (animShake.GetBool("canShake") == true)
+        {
+            StartCoroutine(Shaking());
+        }
+    }
+
+    IEnumerator Shaking()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            animShake.Play("ShakeFreeFall");
+            yield return new WaitForSeconds(0.16f);
+        }
+        animShake.SetBool("canShake", false);
+    }
+}
