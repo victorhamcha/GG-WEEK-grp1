46d66750d5b278a3e5bb4f351a6b01ae90e73453
Hamcha <victor.hamcha@gmail.com>
2020-04-23 19:01:05 +0200

    Shader de merde

--- GGweek/Assets/Script/Victor/NewProjector.cs
+++ GGweek/Assets/Script/Victor/NewProjector.cs
@@ -27,10 +27,10 @@ public class NewProjector : MonoBehaviour
 
     void Update()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
-        Debug.DrawRay(ray.origin, ray.direction, Color.green);
-        Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, projectable);
-        projection.position = new Vector3(hit.point.x+0.001f, hit.point.y + 0.001f, hit.point.z + 0.001f);
+        //Ray ray = new Ray(transform.position, transform.forward);
+        //Debug.DrawRay(ray.origin, ray.direction, Color.green);
+        //Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, projectable);
+        //projection.position = new Vector3(hit.point.x+0.001f, hit.point.y + 0.001f, hit.point.z + 0.001f);
         
         if(near)
         {
