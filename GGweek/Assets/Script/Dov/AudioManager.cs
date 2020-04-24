67f7118f6cc156028096ddddd4114e783036e35c
Benzakk <dovbenzak2@hotmail.com>
2020-04-24 12:23:40 +0200

    Wwise sons (mute)

--- GGweek/Assets/Script/Dov/AudioManager.cs
+++ GGweek/Assets/Script/Dov/AudioManager.cs
@@ -10,58 +10,58 @@ public class AudioManager : MonoBehaviour
     public Slider volumeSFX;
 
 
-    //public void setVolumeGen()
-    //{
-    //    AkSoundEngine.SetRTPCValue("RTPC_Main_Volume", volumeGeneral.value);
-    //}
+    public void setVolumeGen()
+    {
+        AkSoundEngine.SetRTPCValue("RTPC_Main_Volume", volumeGeneral.value);
+    }
 
-    //public void setVolumeMusic()
-    //{
-    //    AkSoundEngine.SetRTPCValue("RTPC_Music_Volume", volumeMusic.value);
-    //}
+    public void setVolumeMusic()
+    {
+        AkSoundEngine.SetRTPCValue("RTPC_Music_Volume", volumeMusic.value);
+    }
 
-    //public void setVolumeSFX()
-    //{
-    //    AkSoundEngine.SetRTPCValue("RTPC_SFX_Volume", volumeSFX.value);
-    //}
+    public void setVolumeSFX()
+    {
+        AkSoundEngine.SetRTPCValue("RTPC_SFX_Volume", volumeSFX.value);
+    }
 
     //public void changeMainMusic()
     //{
-    //    AkSoundEngine.PostEvent("Music_Action", gameObject);
+    //    AkSoundEngine.PostEvent("Play_Music", gameObject);
     //}
 
 
-    //public void Ampoule()
-    //{
-    //    AkSoundEngine.PostEvent("Play_Ampoule", gameObject);
-    //}
+    public void Ampoule()
+    {
+        AkSoundEngine.PostEvent("Play_Ampoule", gameObject);
+    }
 
-    //public void Baleine()
-    //{
-    //    AkSoundEngine.PostEvent("Play_Baleine", gameObject);
-    //}
-    //public void BruitDePas()
-    //{
-    //    AkSoundEngine.PostEvent("Play_Bruit_De_Pas", gameObject);
-    //}
-    //public void CriMonstre()
-    //{
-    //    AkSoundEngine.PostEvent("Play_Cri_Monstre", gameObject);
-    //}
-    //public void Flippant()
-    //{
-    //    AkSoundEngine.PostEvent("Play_flippant", gameObject);
-    //}
-    //public void Tuyau()
-    //{
-    //    AkSoundEngine.PostEvent("Play_Tuyau", gameObject);
-    //}
-    //public void Valve()
-    //{
-    //    AkSoundEngine.PostEvent("Play_Valve", gameObject);
-    //}
-    //public void Vapeur()
-    //{
-    //    AkSoundEngine.PostEvent("Play_Vapeur", gameObject);
-    //}
+    public void Baleine()
+    {
+        AkSoundEngine.PostEvent("Play_Baleine", gameObject);
+    }
+    public void BruitDePas()
+    {
+        AkSoundEngine.PostEvent("Play_Bruit_De_Pas", gameObject);
+    }
+    public void CriMonstre()
+    {
+        AkSoundEngine.PostEvent("Play_Cri_Monstre", gameObject);
+    }
+    public void Flippant()
+    {
+        AkSoundEngine.PostEvent("Play_flippant", gameObject);
+    }
+    public void Tuyau()
+    {
+        AkSoundEngine.PostEvent("Play_Tuyau", gameObject);
+    }
+    public void Valve()
+    {
+        AkSoundEngine.PostEvent("Play_Valve", gameObject);
+    }
+    public void Vapeur()
+    {
+        AkSoundEngine.PostEvent("Play_Vapeur", gameObject);
+    }
 }
